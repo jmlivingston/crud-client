e2c013bc0a95e4fcd68950e31caacc80d3f0ced5
John Livingston <jmlivingston@gmail.com>
2022-11-25 17:07:16 -0800

    Tweak

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
+import { nanoid } from 'nanoid'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { init } from './AppInsights/appInsights'
-import { APP_INSIGHTS } from './CONSTANTS'
+import { APP_INSIGHTS, STORAGE_KEYS } from './CONSTANTS'
 import ErrorBoundary from './ErrorBoundary'
 import './main.scss'
 
-init({ instrumentationKey: APP_INSIGHTS.INSTRUMENTATION_KEY })
+init({
+  instrumentationKey: APP_INSIGHTS.INSTRUMENTATION_KEY,
+  sessionKey: STORAGE_KEYS.APP_INSIGHTS_INFO,
+  sessionId: nanoid(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
